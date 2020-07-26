e7a83706902f34426e814bde996edad87d63df01
aayushi-droid <ayushi98965@gmail.com>
2020-07-26 22:46:54 +0530

    initial commit

---
+++ 1. Very Easy/returnTheNextNumber.cpp
@@ -0,0 +1,19 @@
+/* this program will return next number
+    * Problem statement : Create a function that takes a number as an argument, 
+    * add one to the number, and return the result.    
+    * https://edabit.com/challenge/QgCMMMyMmckvHbfKv
+*/
+
+#include <iostream>
+using namespace std;
+
+int addition(int n) {
+    return n + 1;
+}
+
+int main() {
+    int nextNumber;
+
+    nextNumber = addition(5);
+    cout << nextNumber << endl;
+}
---
+++ 1. Very Easy/sumOfTwoNumbers.cpp
@@ -0,0 +1,18 @@
+/* this program will add two numbers
+    * Problem : https://edabit.com/challenge/SFzHtm63XT6EYNHWY
+*/
+#include <iostream>
+using namespace std;
+
+int addition(int a, int b) {
+    return a + b;
+};
+
+int main() {
+
+    int sum;
+
+    sum = addition(5, 4);
+    cout << sum << endl;
+    return 0;
+}
