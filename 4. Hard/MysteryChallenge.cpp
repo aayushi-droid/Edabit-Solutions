ebbc3d6ad57d3f3185a0313ef4c4d1a6aa42daa0
Jayshah6699 <38597612+Jayshah6699@users.noreply.github.com>
2020-09-26 03:03:32 +0530

    Update MysteryChallenge.cpp

--- 4. Hard/MysteryChallenge.cpp
+++ 4. Hard/MysteryChallenge.cpp
@@ -1,6 +1,6 @@
+```C++
 // Problem statement: Mystery Challenge.
 // Probelm Link: https://edabit.com/challenge/RRX73DEm2BLp8z2TG
-```C++
 //sample test case = 1221
 //sample output = 112211
 #include <bits/stdc++.h>
