86ca6612e29abd05a0ae383f2c992915ccdb47c5
Jayshah6699 <38597612+Jayshah6699@users.noreply.github.com>
2020-09-26 03:02:47 +0530

    Update MysteryChallenge.cpp
    Completed the task

--- 4. Hard/MysteryChallenge.cpp
+++ 4. Hard/MysteryChallenge.cpp
@@ -1,5 +1,34 @@
 // Problem statement: Mystery Challenge.
 // Probelm Link: https://edabit.com/challenge/RRX73DEm2BLp8z2TG
-```c++
+```C++
 //sample test case = 1221
 //sample output = 112211
+#include <bits/stdc++.h>
+using namespace std;
+string mysteryFunc(int n)
+{
+    string str;
+	 string st=to_string(n);;
+    int c=1;
+    for (int i=0; i<st.size(); i++)
+    {
+        if (st[i]==st[i + 1])
+        {
+					c=c+1; 
+		}
+        else
+        {
+    		str=str+to_string(c)+st[i];
+            c=1;
+        }
+    }
+    return str;
+}
+int main()
+{
+    int a=1221;
+    string b=mysteryFunc(a);
+    cout<<b;
+    return 0;
+}
+```
