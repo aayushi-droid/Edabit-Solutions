d7ece7de942d210f73dffc9dfacfa9ae48a12271
Jayanth Jayadevan <jayanth.j737@gmail.com>
2020-10-01 01:12:06 +0530

    Added Convert

---
+++ 1. Very Easy/returnConvert.cpp
@@ -0,0 +1,20 @@
+
+/* Problem-Task : Convert Minutes into Seconds
+ * Problem Link : https://edabit.com/challenge/MtNAJmJZ49i5Xf3AW
+*/
+
+#include <iostream>
+using namespace std;
+
+int convert(int minutes)
+{
+    return 60*minutes;
+}
+
+int main()
+{
+    int minutes;
+    cin>>minutes;
+    cout<<convert(minutes);
+    return 0;
+}
