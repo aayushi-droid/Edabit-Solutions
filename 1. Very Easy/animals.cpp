c2c59ab95513ac144aea613087a205cb6653135b
Avn007 <adityanadkarni007@gmail.com>
2020-10-01 00:06:48 +0530

    Added Description in file

--- 1. Very Easy/animals.cpp
+++ 1. Very Easy/animals.cpp
@@ -1,3 +1,13 @@
+/* Problem statement : In this challenge, a farmer is asking you to tell him how many legs can be counted among all his animals. The farmer breeds three species:
+ chickens = 2 legs
+ cows = 4 legs
+ pigs = 4 legs
+ The farmer has counted his animals and he gives you a subtotal for each species. You have to implement a function that returns the total number of legs of all the animals. */
+
+// Probelm Link :  https://edabit.com/challenge/aADAoRtkbZWEKw9Ap
+// Choose one Cateorgy - Very Easy
+// question category : algorithms, math
+
 #include <iostream>
 
 using namespace std;
