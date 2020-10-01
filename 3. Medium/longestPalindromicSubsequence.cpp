d1c29d9d8b516da9dc3fe4474688f7ad6633f88c
Mohil Khare <mohilkhare17@gmail.com>
2020-10-01 19:33:44 +0530

    Create longestPalindromicSubsequence.cpp
    Basically LCS of string and its reverse

---
+++ 3. Medium/longestPalindromicSubsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lcs(string X, string Y, int m, int n ) {  
+        int L[m + 1][n + 1];  
+        int i, j;  
+        for (i = 0; i <= m; i++) {  
+            for (j = 0; j <= n; j++) {  
+            if (i == 0 || j == 0)  
+                L[i][j] = 0;  
+            else if (X[i - 1] == Y[j - 1])  
+                L[i][j] = L[i - 1][j - 1] + 1;  
+            else
+                L[i][j] = max(L[i - 1][j], L[i][j - 1]);  
+            }  
+        }
+        return L[m][n];  
+    }   
+    int longestPalindromeSubseq(string s) {
+        string srev = s;
+        reverse(srev.begin(), srev.end());
+        return lcs(s, srev, s.length(), s.length());
+    }
+};
