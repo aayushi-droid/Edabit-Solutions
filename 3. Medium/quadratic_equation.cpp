37f182996d39b2e6913c12433e2d4af2e1ee5917
toxic0205 <32976823+Anuj-T@users.noreply.github.com>
2020-10-02 00:09:38 +0530

    Update quadratic_equation.cpp

--- 3. Medium/quadratic_equation.cpp
+++ 3. Medium/quadratic_equation.cpp
@@ -1,6 +1,13 @@
 /*
 Problem Statement
 https://edabit.com/challenge/nasKYub6qEAfQcFuy
+
+Create a function to find only the root value of x in any quadratic equation ax^2 + bx + c. The function will take three arguments:
+
+a as the coefficient of x^2
+b as the coefficient of x
+c as the constant term
+
 */
 
 #include <iostream>
