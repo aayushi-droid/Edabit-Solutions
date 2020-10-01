6a696b71c9f0645c51b392cde06a4164bcb83c06
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-10-01 16:28:42 +0530

    Initial commit for return reminder from two numbers

---
+++ 1. Very Easy/returnreminderfromtwonumbers.cpp
@@ -0,0 +1,17 @@
+// Created by js0805
+#include<iostream>
+using namespace std;
+void solve() {
+	int a, b;
+	cin >> a >> b;
+	cout << a % b << "\n";
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
