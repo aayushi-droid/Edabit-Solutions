cd6dc51ee9f834c723a0289818522a4de0a2d426
saurabh_singh <imsaurabhsingh007@gmail.com>
2020-10-01 18:57:55 +0530

    solved problem that tells whether a number is less than zero or not

---
+++ 1. Very Easy/returnNumberLessThanZero.cpp
@@ -0,0 +1,22 @@
+/* Problem-Task : This program will check whether number is less than zero or not
+ * Problem Link : https://edabit.com/challenge/7KFAJSSp3pmE8bHhK
+*/
+
+#include <iostream>
+using namespace std;
+
+int lessThanOrEqualToZero(int number)
+{
+    if (number <= 0)
+    {
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    cout << lessThanOrEqualToZero(5) << endl;
+    cout << lessThanOrEqualToZero(0) << endl;
+    cout << lessThanOrEqualToZero(2);
+}
