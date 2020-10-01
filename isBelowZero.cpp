80821bc81ebb783b643ce16662f1841a5970944e
uchihacoder <66399743+uchihacoder@users.noreply.github.com>
2020-10-01 16:53:32 +0530

    Create isBelowZero.cpp
    i've written a function which checks if given number is below 0.

---
+++ isBelowZero.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+  // function to check if given number is below zero
+  bool isBelowZero(int input) {
+    if (input <= 0) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+  
+  int main() {
+    // code here
+  }
+    
