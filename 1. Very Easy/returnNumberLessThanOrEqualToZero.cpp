e128189fa3070e6bfa0530d07c373117b8d6787f
ShravyaMallya <mallyashravya@gmail.com>
2020-10-01 18:13:28 +0530

    Commit 1

---
+++ 1. Very Easy/returnNumberLessThanOrEqualToZero.cpp
@@ -0,0 +1,10 @@
+/*Problem statement : Is the Number Less than or Equal to Zero?
+ *Probelm Link : https://edabit.com/challenge/7KFAJSSp3pmE8bHhK
+*/
+
+bool lessThanOrEqualToZero(int num) {
+	if(num<=0)
+		return true;
+	else
+		return false;
+}
