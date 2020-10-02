389b93eb83db71199b764060e8192b99d4a79305
Sangeeta Mishra <mishrasangeeta92@gmail.com>
2020-10-02 19:35:27 +0530

    Create miniPeaks.cpp

---
+++ 3. Medium/miniPeaks.cpp
@@ -0,0 +1,15 @@
+/* Problem-Task : Write a function that returns all the elements in an array that are strictly greater than their adjacent left and right neighbors.
+ * Problem Link : https://edabit.com/challenge/5KAsvzdc9DLfEx65t
+*/
+
+using namespace std;
+
+vector<int> miniPeaks(vector<int> arr) {
+    vector<int> ans;
+    int n = arr.size();
+    for (int i = 1; i < n-1; ++i) {
+            if(arr[i] > arr[i-1] && arr[i] > arr[i+1]) {
+                    ans.push_back(arr[i]);
+            }
+    }
+}
