6d4cf5db383289d6f0b4d0ff32fecad9a63a056e
abhishekjoshi2612 <abhishekjoshi26121999@gmail.com>
2020-10-02 23:15:46 +0530

    changed int to long

--- 1. Very Easy/convertHoursIntoSeconds.cpp
+++ 1. Very Easy/convertHoursIntoSeconds.cpp
@@ -9,7 +9,7 @@ https://edabit.com/challenge/y9Jhs33vdts5GPMW7
 
 #include<bits/stdc++.h>
 using namespace std;
-long howManySeconds(int hours){
+long howManySeconds(long hours){
 	return 60*60*hours;
 }
 int main(){
