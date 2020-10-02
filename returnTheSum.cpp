1049ba1a43154351014c9c2c3e900fde75c0bfaa
anantbharti30 <72209917+anantbharti30@users.noreply.github.com>
2020-10-02 19:59:27 +0530

    easy

---
+++ returnTheSum.cpp
@@ -0,0 +1,6 @@
+/* Problem statement: Create a function that takes two numbers as arguments and return their sum.
+   Problem link: https://edabit.com/challenge/SFzHtm63XT6EYNHWY    */
+#include <iostream>
+int addition(int a, int b) {
+	return a+b;
+}
