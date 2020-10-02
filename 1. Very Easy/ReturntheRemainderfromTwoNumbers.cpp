1f9dbd02f27b95f7f4ac6506e9e13a9bd125cbcd
Jatin Sharma <52196885+js0805@users.noreply.github.com>
2020-10-02 14:42:28 +0530

    create the return the remainder from two numbers

---
+++ 1. Very Easy/ReturntheRemainderfromTwoNumbers.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main() {
+    //declare two variables
+    int a, b;
+    //take input for the two numbers
+    cin >> a >> b;
+    //directly print the remainder
+    cout << a % b << "\n";
+}
