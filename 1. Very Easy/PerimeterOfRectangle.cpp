570fa0254c37247102913279dcf30376ba0c3dd5
rishikasoni67 <53947523+rishikasoni67@users.noreply.github.com>
2020-10-02 11:07:49 +0530

    Add files via upload

---
+++ 1. Very Easy/PerimeterOfRectangle.cpp
@@ -0,0 +1,20 @@
+// ****Task to find Perimeter of a Rectangle****
+// ***Problem link : https://edabit.com/challenge/XnJ24rWW7iJkNrtsh
+
+#include <iostream>
+using namespace std;
+
+int findPerimeter(int l, int b)
+{
+    return (2 * (l + b));
+};
+
+int main()
+{
+    int l, b, perimeter;
+    cout << "Enter the value of length and breadth" << endl;
+    cin >> l >> b;
+    perimeter = findPerimeter(l, b);
+    cout << "The perimeter of rectangle is : " << perimeter << endl;
+    return 0;
+}
