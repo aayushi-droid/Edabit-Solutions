66a075a228bf57bb2c112910cf34479118462126
Abhishek Swaroop Shrivastava <63967165+gh4abhi@users.noreply.github.com>
2020-10-02 21:47:35 +0530

    Added cToA.cpp

---
+++ 1. Very Easy/cToA.cpp
@@ -0,0 +1,23 @@
+
+/* Problem-Task : Char-to-ASCII
+ * Problem Link : https://edabit.com/challenge/TdcPGgdJda6XGJian
+*/
+
+#include <iostream>
+using namespace std;
+
+int ctoa(char c)
+{
+   int n = c;
+   return n;
+}
+
+int main()
+{
+    char c;
+    int n;
+    cin>>c;
+    n = ctoa(c);
+    cout<<n;
+    return 0;
+}
