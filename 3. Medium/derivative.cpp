81f3f2a28a028251cf2615fc0337c7af0c01abb1
Hackerbone <sitaraman.raghav.india@gmail.com>
2020-10-03 16:17:03 +0530

    Derivative of a function program

---
+++ 3. Medium/derivative.cpp
@@ -0,0 +1,26 @@
+/*
+  * This program takes numbers b and m as arguments and returns the derivative of the function f(x)=x^b with respect to x at the point where x=m, where band m are constants.
+  
+  * Problem : https://edabit.com/challenge/sR3aNNQ9kJoSurDLK
+*/
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int derivative(int b, int m)
+{
+    int ans = b * (pow(m, b - 1));
+    return (ans);
+}
+
+int main()
+{
+    int b, m;
+    cout << "Enter exponenet of the function (b) for function f(x) = x^b : ";
+    cin >> b;
+    cout << "\nEnter the value of constant (m) as point x = m : ";
+    cin >> m;
+
+    cout << derivative(b, m);
+    return 0;
+}
