c447cff1d71096bf12c8d60df91511b8edc0b2b8
Abhishek Kujur <59303347+NewCyberGypsy@users.noreply.github.com>
2020-10-04 19:17:21 +0530

    Update gcd.cpp

--- 2.Easy/gcd.cpp
+++ 2.Easy/gcd.cpp
@@ -1,21 +1,36 @@
-  
-/* Problem-Task : Write a function that returns the greatest common divisor (GCD) of two integers.
- * Problem Link : https://edabit.com/challenge/6g5Nj6Qz44Sd2NbBQ
+
+/* 
+   Problem-Task : Write a function that returns the greatest common divisor (GCD) of two integers.
+   Problem Link : https://edabit.com/challenge/6g5Nj6Qz44Sd2NbBQ
 */
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int n1, int n2) {
-	while(n1!=n2){
-		if(n1>n2) n1-=n2;
-		else n2-=n1;
-	}
-	return n1;
+#define ull unsigned long long
+
+ull gcd(ull a, ull b)
+{
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
 }
 
-int main(){
-    cout<<gcd(32,8)<<endl;
-    cout<<gcd(8,12)<<endl;
+int main()
+{
+    ull t;
+    cout << "Enter the number of test case: ";
+    cin >> t;
+    while (t--)
+    {
+        ull a, b;
+        cout << "\nEnter 1st number: ";
+        cin >> a;
+        cout << "\nEnter 2nd number: ";
+        cin >> b;
+        cout << endl;
+        cout << gcd(a, b);
+    }
     return 0;
 }
