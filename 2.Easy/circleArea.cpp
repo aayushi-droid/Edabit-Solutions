298daae92d50fb2a28a79812a23d8badba78d525
Misa Misa <44177887+MisaelSantos@users.noreply.github.com>
2020-10-08 08:57:02 -0300

    Create circleArea.cpp

---
+++ 2.Easy/circleArea.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main () {
+	double A,N=3.14159;
+	
+	cin >> A ;
+	cout<<fixed<<setprecision(4);
+	cout <<"A="<<N * pow(A,2) <<endl;
+	
+	
+	
+}
