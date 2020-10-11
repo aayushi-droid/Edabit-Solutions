68dcfcde5f6c0f774d064ae2e4780149707e1dc1
Ishita Keshawani <50510726+ishitakeshawani@users.noreply.github.com>
2020-10-11 16:38:28 +0530

    Change making problem in CPP (#332)
    * MakingChange in CPP
    
    * Update Making change in cpp

---
+++ 4. Hard/makingchange.cpp
@@ -0,0 +1,29 @@
+//Change Making Problem – Dynamic Programming
+
+#include<bits/stdc++.h> 
+using namespace std;
+
+int minCoins(int coins[], int m, int N)
+{
+  if (N == 0)
+    return 0;
+  int result = INT_MAX;
+  for (int i=0; i<m; i++)
+  {
+    if (coins[i] <= N)
+    {
+      int res = 1 + minCoins(coins, m, N-coins[i]);
+      
+      if (res < result)
+        result = res;
+    }
+  }
+  return result;
+}
+
+int main() {
+  int coins[] = {1,2,3,4,5};
+  int sum = 11; 
+  int total_coins = 5;
+  cout << minCoins(coins,total_coins,sum);
+}
