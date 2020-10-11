83fe65d9bfeca8d74b937490f8a69a9ab0ff4e3f
Himanshu Hansaria <himanshu.hansaria@gmail.com>
2020-10-11 16:30:45 +0530

    Added EDABit solution for Issue-317 (#341)
    * Added EDABit solution for Issue#317
    
    * Rename 2sum.cpp to 2Sum.cpp

---
+++ 1. Very Easy/2Sum.cpp
@@ -0,0 +1,19 @@
+/* Problem-Task : This program will add two numbers
+ * Problem Link : https://edabit.com/challenge/SFzHtm63XT6EYNHWY
+*/
+
+#include<iostream>
+using namespace std;
+int addition(int a, int b) {
+	return a + b;
+}
+
+int main() {
+    int a,b,sum;
+    cin >> a;
+    cin >> b;
+    sum =  addition(a,b);
+    cout << sum;
+    return 0;
+}
+
